991688a669c31b8b986112e18448975bf566235f
kristoperpeipiro12 <kristoperpeipiro@gmail.com>
2024-05-17 01:22:49 +0700

    update

--- public/js/pemasukan.js
+++ public/js/pemasukan.js
@@ -70,38 +70,41 @@
 // });
 
 $(function () {
-    $("#nisInput").keyup(function () {
-      var filter = $(this).val().replace(/\s/g, ""), // Menghapus semua spasi dari nilai input
-        count = 0;
-      $(".recommend .element-rec").each(function () {
-        if (count <= 5 && filter != "") {
+    $("#nisInput").on("input", function () {
+      var inputVal = $(this).val().replace(/\s/g, ""); // Menghapus semua spasi dari nilai input
+      var count = 0;
+
+      // Jika input tidak kosong
+      if (inputVal !== "") {
+        $(".recommend .element-rec").each(function () {
           var text = $(this).text().replace(/\s/g, ""); // Menghapus semua spasi dari teks elemen
-          if (text.search(new RegExp(`(?<=${filter}).*`, "i")) < 0) {
-            $(this).removeClass("d-flex").addClass("d-none");
-          } else {
+          if (text.includes(inputVal)) {
             $(this).removeClass("d-none").addClass("d-flex");
             count++;
+          } else {
+            $(this).removeClass("d-flex").addClass("d-none");
           }
-        } else {
-          $(".element-rec").removeClass("d-flex").addClass("d-none");
-        }
-      });
+          if (count >= 5) return false; // Hentikan iterasi setelah menemukan 5 rekomendasi
+        });
 
-      if (count === 0 && filter != "") {
-        $("#not-found").removeClass("d-none").addClass("d-flex");
+        if (count === 0) {
+          $("#not-found").removeClass("d-none").addClass("d-flex");
+        } else {
+          $("#not-found").removeClass("d-flex").addClass("d-none");
+        }
       } else {
+        // Jika input kosong, sembunyikan semua rekomendasi dan pesan tidak ditemukan
+        $(".element-rec").removeClass("d-flex").addClass("d-none");
         $("#not-found").removeClass("d-flex").addClass("d-none");
       }
     });
 
     $(".element-rec").click(function () {
-        let value = $(this).text().replace(/\s/g, "");
-        let nisInput =  $("#nisInput").val(value);
-        console.log(nisInput);
-        if (nisInput == value) {
-        }
-       // Menghapus semua spasi dari teks elemen yang diklik
-      $(this).removeClass("d-flex").addClass("d-none");
+      var value = $(this).text().replace(/\s/g, ""); // Menghapus semua spasi dari teks elemen yang diklik
+      $("#nisInput").val(value);
+      $(".element-rec").removeClass("d-flex").addClass("d-none");
       $("#not-found").removeClass("d-flex").addClass("d-none");
     });
   });
+
+
