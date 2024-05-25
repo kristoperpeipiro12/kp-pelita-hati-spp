0d79524bd5ef94cbb8a655d229955a4645fb3a56
kristoperpeipiro03@gmail.com <kristoperpeipiro03@gmail.com>
2024-05-25 13:10:13 +0700

    upp

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [LoginController::class, 'index'])->name('login');
 Route::post('/login-proses', [LoginController::class, 'login_proses'])->name('login-proses');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-// Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
+Route::get('/logout', [LoginController::class, 'logout'])->name('logout');
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth:web', 'role:admin']], function () {
     Route::get('', [HomeController::class, 'admin'])->name('admin');
